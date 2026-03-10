267f634be3a16833674745b1cf11f14ffb7169f3
Kiril Bogza <sobacika1999@gmail.com>
2026-03-10 14:21:15 +0100

    feat: add server-module

---
+++ src/index.ts
@@ -0,0 +1,50 @@
+import fastify, { FastifyInstance } from 'fastify';
+import cors from '@fastify/cors';
+import helmet from '@fastify/helmet';
+import rateLimit from '@fastify/rate-limit';
+import auth from './auth';               
+import adminRoutes from './routes/admin';
+import partnerRoutes from './routes/partner'; // skapar struktur i förväg, from draw.io
+import publicRoutes from './routes/public';
+import { connectDB, getPool } from './db';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const server: FastifyInstance = fastify({ logger: true });
+const port = process.env.PORT ? parseInt(process.env.PORT) : 3000;
+
+async function start() {
+  try {
+    await connectDB();
+    const pool = getPool();
+
+    // decorate with pg pool
+    server.decorate('pg', pool);
+
+    server.addHook('onClose', async () => {
+      await pool.end();
+    });
+
+    await server.register(cors, {});
+    await server.register(helmet, {});
+    await server.register(rateLimit, {
+      max: 100,
+      timeWindow: '1 minute'
+    });
+
+    await server.register(auth);
+
+    await server.register(publicRoutes);   //health check 🟢
+    await server.register(adminRoutes, { prefix: '/admin' });
+    await server.register(partnerRoutes, { prefix: '/partner' });
+
+    await server.listen({ host: '0.0.0.0', port });
+    console.log(`listening at http://localhost:${port}`);
+  } catch (err) {
+    server.log.error(err);
+    process.exit(1);
+  }
+}
+
+start();
